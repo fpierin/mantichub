3015b3fe18d54b5d6d4712a9585e7b151379a683
Daniel Deimann <daniel.deimann@gmail.com>
2017-04-09 23:19:48 -0300

    Adição de rotas | Criação do componentes de mapa e header

--- mantic-client/src/app/app.module.ts
+++ mantic-client/src/app/app.module.ts
@@ -3,7 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { MapsModule } from './maps/maps.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MapsModule,
+    SharedModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
